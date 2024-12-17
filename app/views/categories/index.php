034e7ffd81c753259ac8deea74f70c7d6abdf272
Monakyaa <dewimn.rizki@gmail.com>
2024-12-17 10:45:51 +0700

    daftar kategori

--- app/views/categories/index.php
+++ app/views/categories/index.php
@@ -1,16 +1,60 @@
 <!-- app/views/categories/index.php -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-<h2>Daftar Kategori</h2>
-<a href="/categories/create">Tambah Kategori</a>
-<ul>
-    <?php foreach ($categories as $kategori): ?>
-        <div>
-            <p><?= htmlspecialchars($kategori['nama_kategori']) ?> - <?= htmlspecialchars($kategori['deskripsi']) ?>
-            <a href="/categories/edit/<?php echo $kategori['id_kategori']; ?>">Edit</a> |
-            <a href="/categories/delete/<?php echo $kategori['id_kategori']; ?>" onclick="return confirm('Anda Yakin?')">Delete</a>
-            </p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Kategori</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+</head>
+<body>
+    <div class="container" style="margin-top: 80px">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>DAFTAR KATEGORI</h4>
+                    </div>
+                    <div class="card-body">
+                        <a href="/categories/create" class="btn btn-success mb-3">Tambah Kategori</a>
+                        <table class="table table-bordered" id="myTable">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Kategori</th>
+                                    <th scope="col">Deskripsi</th>
+                                    <th scope="col">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($categories as $kategori): ?>
+                                    <tr>
+                                        <td><?= htmlspecialchars($kategori['nama_kategori']) ?></td>
+                                        <td><?= htmlspecialchars($kategori['deskripsi']) ?></td>
+                                        <td>
+                                            <a href="/categories/edit/<?php echo $kategori['id_kategori']; ?>" class="btn btn-sm btn-primary">Edit</a>
+                                            <a href="/categories/delete/<?php echo $kategori['id_kategori']; ?>" onclick="return confirm('Anda Yakin?')" class="btn btn-sm btn-danger">Delete</a>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
-    <?php endforeach; ?>
-</ul>
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    </div>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <script src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+
+    <!-- DataTables Initialization -->
+    <script>
+        $(document).ready(function() {
+            $('#myTable').DataTable();
+        });
+    </script>
+</body>
+</html>
