5b52eea4607eeb63e5eab1b2abfe59a3eef083d3
HanaKurnia <hanakurnia.stu@pnc.ac.id>
2024-12-17 10:09:25 +0700

    2

--- app/views/order/create.php
+++ app/views/order/create.php
@@ -1,15 +1,36 @@
 <!-- app/views/order/create.php -->
-<h2>Tambah Orderan Baru</h2>
-<form action="/order/store" method="POST">
-<label for="id_tanaman">Nama Tanaman:</label>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <title>Tambah Siswa</title>
+  </head>
+
+  <body>
+
+    <div class="container" style="margin-top: 80px">
+      <div class="row">
+        <div class="col-md-8 offset-md-2">
+          <div class="card">
+            <div class="card-header">
+              TAMBAH ORDER BARU
+        </div>
+        <div class="card-body">
+        <form action="/order/store" method="POST">
+        <div class="form-group">
+        <label for="id_tanaman">Nama Tanaman:</label>
     <select name="id_tanaman" id="">
         <option value=""></option>
         <?php foreach ($plants as $p): ?>
-            <option value="<?=$p['id_tanaman'] ?>"><?=$p['nama_tanaman']?></option>
-            <?php endforeach; ?>
+        <option value="<?=$p['id_tanaman'] ?>"><?=$p['nama_tanaman']?></option>
+        <?php endforeach; ?>
     </select>
-    <br>
-    <label for="id_user">Pembeli:</label>
+        <br>
+
+        <div class="form-group">
+        <label for="id_user">Pembeli:</label>
     <select name="id_user" id="">
         <option value=""></option>
         <?php foreach ($users as $u): ?>
@@ -19,6 +40,18 @@
     <br>
     <label for="status_pesanan">Status Pesanan:</label>
     <input type="text" name="status_pesanan" id="status_pesanan" required>
-    <br>
-    <button type="submit">Simpan</button>
-</form>
+
+                <button type="submit" class="btn btn-success">SIMPAN</button>
+                <button type="reset" class="btn btn-warning">RESET</button>
+
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  </body>
+</html>
--- app/views/order/edit.php
+++ app/views/order/edit.php
@@ -10,18 +10,22 @@
     <h2>Edit Order</h2>
     <form action="/order/update/<?php echo $order['id_order'];?>" method="POST">
     <label for="id_tanaman">Nama Tanaman:</label>
-    <select name="id_tanaman" id="">
-        <option value=""><?=$order['nama_tanaman']?></option>
-        <?php foreach ($plants as $p): ?>
-            <option value="<?=$p['id_tanaman'] ?>"><?=$p['nama_tanaman']?></option>
+    <select name="id_tanaman" id="id_tanaman">
+            <?php foreach ($plants as $p): ?>
+                <option value="<?php echo $p['id_tanaman']; ?>"
+                    <?php echo ($p['id_tanaman'] == $order['id_tanaman']) ? 'selected' : ''; ?>>
+                    <?php echo htmlspecialchars($p['nama_tanaman']); ?>
+                </option>
             <?php endforeach; ?>
-    </select>
+        </select>
     <br>
     <label for="id_user">Pembeli:</label>
-    <select name="id_user" id="">
-        <option value=""><?=$order['nama']?></option>
+    <select name="id_user" id="id_user">
         <?php foreach ($users as $u): ?>
-            <option value="<?=$u['id_user'] ?>"><?=$u['nama']?></option>
+            <option value="<?php echo $u['id_user']; ?>"
+            <?php echo ($u['id_user'] == $order['id_user']) ? 'selected' : ''; ?>>
+            <?php echo htmlspecialchars($u['nama']); ?>
+            </option>
             <?php endforeach; ?>
     </select>
     <br>
